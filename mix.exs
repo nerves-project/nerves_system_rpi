877d856a729ad1815f4a5be31baad4f99114f3a7
Justin Schneck <jschneck@mac.com>
2017-11-19 22:57:03 -0500

    update to nerves 0.8 and nerves_system_br 0.15.0

--- mix.exs
+++ mix.exs
@@ -1,30 +1,50 @@
 defmodule NervesSystemRpi.Mixfile do
   use Mix.Project
 
+  @app :nerves_system_rpi
   @version Path.join(__DIR__, "VERSION")
     |> File.read!
     |> String.trim
 
   def project do
-    [app: :nerves_system_rpi,
-     version: @version,
-     elixir: "~> 1.3",
-     compilers: Mix.compilers ++ [:nerves_package],
-     description: description(),
-     package: package(),
-     deps: deps(),
-     aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]]
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.4",
+      compilers: Mix.compilers ++ [:nerves_package],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]
+    ]
   end
 
   def application do
     []
   end
 
+  def nerves_package do
+    [
+      type: :system,
+      version: @version,
+      compiler: :nerves_package,
+      artifact_url: [
+        "https://github.com/nerves-project/#{@app}/releases/download/v#{@version}/#{@app}-v#{@version}.tar.gz",
+      ],
+      platform: Nerves.System.BR,
+      platform_config: [
+        defconfig: "nerves_defconfig"
+      ],
+      checksum: package_files()
+    ]
+  end
+
   defp deps do
     [
-      {:nerves, "~> 0.7", runtime: false },
-      {:nerves_system_br, "~> 0.14.1", runtime: false },
-      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.11.0", runtime: false}
+      {:nerves, "~> 0.8", runtime: false},
+      {:nerves_system_br, "~> 0.15.0", runtime: false, app: false},
+      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.12.0", runtime: false, app: false}
     ]
   end
 
@@ -35,9 +55,27 @@ defmodule NervesSystemRpi.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Frank Hunleth", "Justin Schneck"],
-    files: ["LICENSE", "mix.exs", "nerves_defconfig", "nerves.exs", "README.md", "VERSION", "rootfs_overlay", "fwup.conf", "cmdline.txt", "linux-4.4.defconfig", "config.txt", "post-createfs.sh"],
-     licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/nerves-project/nerves_system_rpi"}]
+    [
+      maintainers: ["Frank Hunleth", "Justin Schneck"],
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/nerves-project/#{@app}"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "LICENSE",
+      "mix.exs",
+      "nerves_defconfig",
+      "README.md",
+      "VERSION",
+      "rootfs_overlay",
+      "fwup.conf",
+      "cmdline.txt", 
+      "linux-4.4.defconfig",
+      "config.txt",
+      "post-createfs.sh"
+    ]
   end
 end
